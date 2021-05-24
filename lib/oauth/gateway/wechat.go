a5b5315f38d27e006c3bc5b3aac551b04e81c637
xuzhou <xuzhou@chinamcloud.com>
2021-05-24 20:11:40 +0800

    添加微博、微信、qq互联

---
+++ lib/oauth/gateway/wechat.go
@@ -0,0 +1,135 @@
+package gateway
+
+import (
+	"fmt"
+	"github.com/idoubi/goz"
+	"net/url"
+	"pmsGo/app/model"
+	"pmsGo/app/service"
+	"pmsGo/lib/config"
+	"pmsGo/lib/security/base64"
+	"pmsGo/lib/security/encrypt"
+)
+
+const WechatGatewayType = "wechat"
+const WechatScopeType = "snsapi_login"
+const WechatGrantType = "authorization_code"
+const (
+	WechatAuthorizeUrl   = "https://open.weixin.qq.com/connect/qrconnect"
+	WechatAccessTokenUrl = "https://api.weixin.qq.com/sns/oauth2/access_token"
+	WechatAccessUserUrl  = "https://api.weixin.qq.com/sns/userinfo"
+)
+
+type WechatAccessTokenRequest struct {
+	Appid     string `json:"appid"`
+	Secret    string `json:"secret"`
+	Code      string `json:"code"`
+	GrantType string `json:"grant_type"`
+}
+
+type Wechat struct {
+	WechatOpenId    string
+	WechatAppId     string
+	WechatAppSecret string
+}
+
+func NewWechat() (*Wechat, error) {
+	gateway := &Wechat{}
+	appId := service.SettingService.GetSetting(model.SettingKeyWechatAppId)
+	if appId == "" {
+		return nil, fmt.Errorf("缺少配置：%v", model.SettingKeyWechatAppId)
+	}
+	gateway.WechatAppId = appId
+	appSecret := service.SettingService.GetSetting(model.SettingKeyWechatAppSecret)
+	if appSecret == "" {
+		return nil, fmt.Errorf("缺少配置：%v", model.SettingKeyWechatAppSecret)
+	}
+	decrypt, err := encrypt.Decrypt(base64.Decode(appSecret), []byte(config.Config.Web.Security["salt"]))
+	if err != nil {
+		return nil, err
+	}
+	gateway.WechatAppSecret = string(decrypt)
+	return gateway, nil
+}
+
+func (gateway Wechat) Scope() string {
+	return WechatScopeType
+}
+
+func (gateway Wechat) GrantType() string {
+	return WechatGrantType
+}
+
+func (gateway Wechat) AuthorizeUrl(scope string, redirect string, state string) string {
+	if scope == "" {
+		scope = gateway.Scope()
+	}
+	url := url.URL{}
+	query := url.Query()
+	query.Add("appid", gateway.WechatAppId)
+	query.Add("redirect_uri", redirect)
+	query.Add("scope", scope)
+	query.Add("state", state)
+	query.Add("response_type", "code")
+	queryString := query.Encode()
+	return WechatAuthorizeUrl + "?" + queryString + "#wechat_redirect"
+}
+
+func (gateway *Wechat) AccessToken(code string, redirect string, state string) (string, error) {
+	requestData := &WechatAccessTokenRequest{
+		Appid:     gateway.WechatAppId,
+		Secret:    gateway.WechatAppSecret,
+		GrantType: gateway.GrantType(),
+		Code:      code,
+	}
+	client := goz.NewClient()
+	response, err := client.Post(WechatAccessTokenUrl, goz.Options{
+		Headers: map[string]interface{}{
+			"Content-Type": "application/json",
+			"Accept":       "application/json",
+		},
+		JSON: requestData,
+	})
+	if err != nil {
+		return "", err
+	}
+	body, err := response.GetParsedBody()
+	if err != nil {
+		return "", err
+	}
+	gateway.WechatOpenId = body.Get("openid").String()
+	return body.Get("access_token").String(), nil
+}
+
+func (gateway Wechat) User(accessToken string) (map[string]string, error) {
+	client := goz.NewClient()
+	response, err := client.Get(WechatAccessUserUrl, goz.Options{
+		Headers: map[string]interface{}{
+			"Content-Type": "application/json",
+			"Accept":       "application/json",
+		},
+		Query: map[string]string{
+			"access_token": accessToken,
+			"openid":       gateway.WechatOpenId,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	body, err := response.GetParsedBody()
+	if err != nil {
+		return nil, err
+	}
+	sex := "2"
+	if body.Get("gender").String() == "1" {
+		sex = "1"
+	}
+	return map[string]string{
+		"avatar":   body.Get("headimgurl").String(),
+		"channel":  "0",
+		"nickname": body.Get("nickname").String(),
+		"gender":   sex,
+		"open_id":  gateway.WechatOpenId,
+		"union_id": body.Get("unionid").String(),
+	}, nil
+}
--- lib/oauth/gateway/weibo.go
+++ lib/oauth/gateway/weibo.go
@@ -1,13 +1,149 @@
 package gateway
 
+import (
+	"fmt"
+	"github.com/idoubi/goz"
+	"net/url"
+	"pmsGo/app/model"
+	"pmsGo/app/service"
+	"pmsGo/lib/config"
+	"pmsGo/lib/security/base64"
+	"pmsGo/lib/security/encrypt"
+)
+
+const WeiboGatewayType = "weibo"
+
+const WeiboScopeType = "all"
+const WeiboGrantType = "authorization_code"
+const WeiboAuthorizeDisplay = "default"
+const (
+	WeiboAuthorizeUrl   = "https://api.weibo.com/oauth2/authorize"
+	WeiboAccessTokenUrl = "https://api.weibo.com/oauth2/access_token"
+	WeiboAccessUserUrl  = "https://api.weibo.com/2/users/show.json"
+)
+
+type WeiboAccessTokenRequest struct {
+	ClientId     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+	GrantType    string `json:"grant_type"`
+	RedirectUri  string `json:"redirect_uri"`
+	Code         string `json:"code"`
+}
+type WeiboAccessTokenResponse struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn   string `json:"expires_in"`
+	RemindIn    string `json:"remind_in"`
+	Uid         string `json:"uid"`
+}
+type WeiboAccessUserRequest struct {
+	AccessToken string `json:"access_token"`
+	Uuid        string `json:"uuid"`
+}
 type Weibo struct {
+	WeiboUuid      string
+	WeiboAppKey    string
+	WeiboAppSecret string
 }
 
 func NewWeibo() (*Weibo, error) {
 	gateway := &Weibo{}
-
+	appId := service.SettingService.GetSetting(model.SettingKeyWeiboAppId)
+	if appId == "" {
+		return nil, fmt.Errorf("缺少配置：%v", model.SettingKeyWeiboAppId)
+	}
+	gateway.WeiboAppKey = appId
+	appSecret := service.SettingService.GetSetting(model.SettingKeyWeiboAppSecret)
+	if appSecret == "" {
+		return nil, fmt.Errorf("缺少配置：%v", model.SettingKeyWeiboAppSecret)
+	}
+	decrypt, err := encrypt.Decrypt(base64.Decode(appSecret), []byte(config.Config.Web.Security["salt"]))
+	if err != nil {
+		return nil, err
+	}
+	gateway.WeiboAppSecret = string(decrypt)
 	return gateway, nil
 }
-func (gateway Weibo) AuthorizeUrl() {
 
+func (gateway Weibo) Scope() string {
+	return WeiboScopeType
+}
+
+func (gateway Weibo) GrantType() string {
+	return WeiboGrantType
+}
+
+func (gateway Weibo) AuthorizeUrl(scope string, redirect string, state string) string {
+	if scope == "" {
+		scope = gateway.Scope()
+	}
+	url := url.URL{}
+	query := url.Query()
+	query.Add("client_id", gateway.WeiboAppKey)
+	query.Add("redirect_uri", redirect)
+	query.Add("scope", scope)
+	query.Add("state", state)
+	query.Add("display", WeiboAuthorizeDisplay)
+	query.Add("forcelogin", "true")
+	queryString := query.Encode()
+	return WeiboAuthorizeUrl + "?" + queryString
+}
+
+func (gateway *Weibo) AccessToken(code string, redirect string, state string) (string, error) {
+	requestData := &WeiboAccessTokenRequest{
+		ClientId:     gateway.WeiboAppKey,
+		ClientSecret: gateway.WeiboAppSecret,
+		GrantType:    gateway.GrantType(),
+		Code:         code,
+		RedirectUri:  redirect,
+	}
+	client := goz.NewClient()
+	response, err := client.Post(WeiboAccessTokenUrl, goz.Options{
+		Headers: map[string]interface{}{
+			"Content-Type": "application/json",
+			"Accept":       "application/json",
+		},
+		JSON: requestData,
+	})
+	if err != nil {
+		return "", err
+	}
+	body, err := response.GetParsedBody()
+	if err != nil {
+		return "", err
+	}
+	gateway.WeiboUuid = body.Get("uuid").String()
+	return body.Get("access_token").String(), nil
+}
+
+func (gateway Weibo) User(accessToken string) (map[string]string, error) {
+	client := goz.NewClient()
+	response, err := client.Get(WeiboAccessUserUrl, goz.Options{
+		Headers: map[string]interface{}{
+			"Content-Type": "application/json",
+			"Accept":       "application/json",
+		},
+		Query: map[string]string{
+			"access_token": accessToken,
+			"uuid":         gateway.WeiboUuid,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	body, err := response.GetParsedBody()
+	if err != nil {
+		return nil, err
+	}
+	sex := "2"
+	if body.Get("gender").String() == "m" {
+		sex = "1"
+	}
+	return map[string]string{
+		"avatar":   body.Get("avatar_hd").String(),
+		"channel":  "0",
+		"nickname": body.Get("screen_name").String(),
+		"gender":   sex,
+		"open_id":  gateway.WeiboUuid,
+		"union_id": gateway.WeiboUuid,
+	}, nil
 }
