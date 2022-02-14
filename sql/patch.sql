d57c04c3cb94528be87d1f6b7a02ef2fec94dbd8
xuzhou <xuzhou@chinamcloud.com>
2022-02-14 20:27:39 +0800

    文件添加视频封面图字段

--- sql/patch.sql
+++ sql/patch.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `pms_files`
     ADD COLUMN `status` int NOT NULL COMMENT '状态，0上传成功，1文件处理中，2可用' AFTER `preview`;
+
+ALTER TABLE `pms_files`
+    ADD COLUMN `poster` varchar(255) NULL COMMENT '封面图' AFTER `name`;
